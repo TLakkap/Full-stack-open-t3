174fe57558f5a035c38e148b0175376f1ea02214
Tiina Lakkapää <t0lati02@students.oamk.fi>
2023-11-17 14:56:02 +0200

    exerc 3.5-3.6

--- index.js
+++ index.js
@@ -54,6 +54,40 @@ app.delete('/api/persons/:id', (req, res) => {
     res.status(204).end()
 })
 
+const generateId = () => {
+    return Math.floor(Math.random() * 1000);
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+    
+    const nameIndex = persons.findIndex(person => person.name === body.name)   
+
+    if(!body.name) {
+        return res.status(400).json({
+            error: 'name missing'
+        })
+    }else if(!body.number) {
+        return res.status(400).json({
+            error: 'number missing'
+        })
+    }else if(nameIndex !== -1) {
+        return res.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+    
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+
+    persons = persons.concat(person)
+
+    res.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
