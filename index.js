fa2a6a68ce58f194edd9d886c6c74aae4013e289
TLakkap <tiina.lakkapaa@gmail.com>
2023-11-18 23:57:16 +0200

    exerc 3.17

--- index.js
+++ index.js
@@ -28,29 +28,6 @@ morgan.token('requestData', (req) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :requestData'))
 
-/* let persons = [
-    {
-      name: "Arto Hellas",
-      number: "040-123456",
-      id: 1
-    },
-    {
-      name: "Ada Lovelace",
-      number: "39-44-5323523",
-      id: 2
-    },
-    {
-      name: "Dan Abramov",
-      number: "12-43-234345",
-      id: 3
-    },
-    {
-      name: "Mary Poppendieck",
-      number: "39-23-6423122",
-      id: 4
-    }
-  ] */
-
 app.get('/api/persons', (req, res) => {
     Person.find({}).then(persons => {
         res.json(persons)
@@ -83,28 +60,8 @@ app.delete('/api/persons/:id', (req, res, next) => {
         .catch(error => next(error))
 })
 
-/* const generateId = () => {
-    return Math.floor(Math.random() * 1000);
-} */
-
 app.post('/api/persons', (req, res, next) => {
     const body = req.body
-    
-    /* const nameIndex = persons.findIndex(person => person.name === body.name)   
-
-    if(!body.name) {
-        return res.status(400).json({
-            error: 'name missing'
-        })
-    }else if(!body.number) {
-        return res.status(400).json({
-            error: 'number missing'
-        })
-    }else if(nameIndex !== -1) {
-        return res.status(400).json({
-            error: 'name must be unique'
-        })
-    } */
     
     const person = new Person({
         name: body.name,
@@ -114,6 +71,21 @@ app.post('/api/persons', (req, res, next) => {
     person.save()
         .then(savedPerson => {
             res.json(savedPerson)
+        })
+        .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(req.params.id, person,{ new: true })  //{ new true } is added because otherwise the object before updating would be returned
+        .then(updatedPerson => {
+            res.json(updatedPerson)
         })
         .catch(error => next(error))
 })
