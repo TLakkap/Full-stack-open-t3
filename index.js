08bf4bb0ffda79645dc65d5de0b72dd980c2e8e1
TLakkap <tiina.lakkapaa@gmail.com>
2023-11-19 10:41:54 +0200

    exerc 3.19

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const errorHandler = (error, request, response, next) => {
   
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
-    }  
+    } else if (error.name === 'ValidationError'){
+        return response.status(400).json({ error: error.message })
+    }
     next(error)
 }
 
@@ -77,14 +79,18 @@ app.post('/api/persons', (req, res, next) => {
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const body = req.body
+    const { name, number } = req.body
 
-    const person = {
+    /* const person = {
         name: body.name,
         number: body.number,
-    }
+    } */
 
-    Person.findByIdAndUpdate(req.params.id, person,{ new: true })  //{ new true } is added because otherwise the object before updating would be returned
+    Person.findByIdAndUpdate(
+        req.params.id, 
+        { name, number },
+        { new: true, runValidators: true, context: 'query' }  //{ new true } is added because otherwise the object before updating would be returned
+        )
         .then(updatedPerson => {
             res.json(updatedPerson)
         })
