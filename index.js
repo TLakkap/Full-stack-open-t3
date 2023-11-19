41a092b339eab5d3d0282285c78a1dab5663d07e
TLakkap <tiina.lakkapaa@gmail.com>
2023-11-19 15:02:37 +0200

    exerc 3.20-3.21

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ const Person = require('./models/person')
 
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
+    console.log(error.name)
   
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
     } else if (error.name === 'ValidationError'){
         return response.status(400).json({ error: error.message })
+    } else if (error.name === 'ReferenceError'){
+        return response.status(404).json({error: error.message})
     }
     next(error)
 }
@@ -81,18 +84,17 @@ app.post('/api/persons', (req, res, next) => {
 app.put('/api/persons/:id', (req, res, next) => {
     const { name, number } = req.body
 
-    /* const person = {
-        name: body.name,
-        number: body.number,
-    } */
-
     Person.findByIdAndUpdate(
         req.params.id, 
         { name, number },
         { new: true, runValidators: true, context: 'query' }  //{ new true } is added because otherwise the object before updating would be returned
         )
         .then(updatedPerson => {
-            res.json(updatedPerson)
+            if(updatedPerson !== null){
+                res.json(updatedPerson)
+            } else {
+                next(error)
+            }
         })
         .catch(error => next(error))
 })
